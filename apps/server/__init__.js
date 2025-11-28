6ae67c41975ce731cf0d8b5b8a31738fb14dfd8b
Jared Muñoz <amunozbaez669@gmail.com>
2025-11-28 15:02:00 -0600

    feat: now into a monorepo with turborepo

---
+++ apps/server/__init__.js
@@ -0,0 +1,16 @@
+const { spawn } = require("node:child_process");
+
+// COMMAND: venv/Scripts/activate && python ./app.py
+
+const pythonProcess = spawn("../../venv/Scripts/python.exe", ["./app.py"], {
+    stdio: "inherit",
+    cwd: __dirname,
+});
+
+pythonProcess.on("close", (code) => {
+    console.log(`Python process exited with code ${code}`);
+});
+
+pythonProcess.on("error", (error) => {
+    console.error(`Python process error: ${error.message}`);
+});
